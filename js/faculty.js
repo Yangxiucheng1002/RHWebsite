1d8e5877c33c4e85197c4d69625689174f4e5bb6
yangxiucheng <1662566616@qq.com>
2020-04-11 14:34:43 +0800

    style:提交了师资力量自适应

--- js/faculty.js
+++ js/faculty.js
@@ -1,11 +1,11 @@
 window.onload = function () {
     getTeacherMessage();
-
     function getTeacherMessage() {
         const teacher_1 = document.getElementById('teacher_1');
         const teacher_2 = document.getElementById('teacher_2');
         const first_img = document.getElementsByClassName('first-img')[0];
         const last_img = document.getElementsByClassName('last-img')[0];
+
         first_img.onmousemove = function (ev) {
             if (ev) {
                 teacher_2.style.display = 'none';
@@ -19,6 +19,5 @@ window.onload = function () {
                 teacher_1.style.display = 'none';
             }
         }
-
     }
 };
